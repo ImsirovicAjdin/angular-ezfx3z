99ad8ecb0f5ec22c5de77fc0362015ea24c9ba7f
ImsirovicAjdin <ajdika@live.com>
2019-07-16 13:12:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ezfx3z
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ezfx3z)
---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+// Render an Observable with the Async Pipe
+
+import { Component } from '@angular/core';
+import {Observable} from 'rxjs/Observable'
+
+@Component({
+  selector: 'my-app',
+  //templateUrl: './app.component.html',
+  //styleUrls: [ './app.component.css' ]
+  template: `
+    <h1>{{message}}</h1>
+  `
+})
+export class AppComponent  {
+  //name = 'Hello, world';
+  clock = Observable.interval(1000); // Observable with interval of 1 sec
+  // clock = Observable.interval(1000) throws: 
+  // ERROR Error: Observable_1.Observable.interval is not a function
+  // That means you need to import every method on Observable by itself
+}
